2a5a39d472e72b3981cef4566d4362b88b2b9c54
Sergey <sergey1fox@gmail.com>
2024-03-11 01:53:42 +0500

    Initial state

---
+++ Source/zcl_watercounter.h
@@ -0,0 +1,127 @@
+/**************************************************************************************************
+  Filename:       zcl_watercounter.h
+  Revised:        $Date: 2015-08-19 17:11:00 -0700 (Wed, 19 Aug 2015) $
+  Revision:       $Revision: 44460 $
+
+
+  Description:    This file contains the Zigbee Cluster Library Home
+                  Automation Sample Application.
+
+
+  Copyright 2006-2013 Texas Instruments Incorporated. All rights reserved.
+
+  IMPORTANT: Your use of this Software is limited to those specific rights
+  granted under the terms of a software license agreement between the user
+  who downloaded the software, his/her employer (which must be your employer)
+  and Texas Instruments Incorporated (the "License").  You may not use this
+  Software unless you agree to abide by the terms of the License. The License
+  limits your use, and you acknowledge, that the Software may not be modified,
+  copied or distributed unless embedded on a Texas Instruments microcontroller
+  or used solely and exclusively in conjunction with a Texas Instruments radio
+  frequency transceiver, which is integrated into your product.  Other than for
+  the foregoing purpose, you may not use, reproduce, copy, prepare derivative
+  works of, modify, distribute, perform, display or sell this Software and/or
+  its documentation for any purpose.
+
+  YOU FURTHER ACKNOWLEDGE AND AGREE THAT THE SOFTWARE AND DOCUMENTATION ARE
+  PROVIDED “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+  INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY, TITLE,
+  NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL
+  TEXAS INSTRUMENTS OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER CONTRACT,
+  NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR OTHER
+  LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+  INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE
+  OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT
+  OF SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+  (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+
+  Should you have any questions regarding your right to use this Software,
+  contact Texas Instruments Incorporated at www.TI.com.
+**************************************************************************************************/
+
+#ifndef ZCL_WATERCOUNTER_H
+#define ZCL_WATERCOUNTER_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/*********************************************************************
+ * INCLUDES
+ */
+#include "zcl.h"
+
+/*********************************************************************
+ * CONSTANTS
+ */
+#define WATERCOUNTER_ENDPOINT               8
+
+// Events for the sample app
+#define SAMPLEAPP_END_DEVICE_REJOIN_EVT   0x0001
+
+// UI Events
+#define SAMPLEAPP_LCD_AUTO_UPDATE_EVT       0x0010  
+#define SAMPLEAPP_KEY_AUTO_REPEAT_EVT       0x0020  
+
+#define SAMPLEAPP_END_DEVICE_REJOIN_DELAY 10000
+
+/*********************************************************************
+ * MACROS
+ */
+/*********************************************************************
+ * TYPEDEFS
+ */
+
+/*********************************************************************
+ * VARIABLES
+ */
+extern SimpleDescriptionFormat_t zclWaterCounter_SimpleDesc;
+
+extern SimpleDescriptionFormat_t zclWaterCounter9_SimpleDesc;
+
+extern CONST zclAttrRec_t zclWaterCounter_Attrs[];
+
+extern uint8    zclWaterCounter_Flow1Desc[];
+extern uint32   zclWaterCounter_Flow1Value;
+extern uint16   zclWaterCounter_Flow1Multiplyer;
+extern uint8    zclWaterCounter_Flow1Unit[];
+extern uint8    zclWaterCounter_Flow1Status;
+
+extern uint8    zclWaterCounter_Flow2Desc[];
+extern uint32   zclWaterCounter_Flow2Value;
+extern uint16   zclWaterCounter_Flow2Multiplyer;
+extern uint8    zclWaterCounter_Flow2Unit[];
+extern uint8    zclWaterCounter_Flow2Status;
+
+extern uint16   zclWaterCounter_IdentifyTime;
+
+extern CONST uint8 zclWaterCounter_NumAttributes;
+
+/*********************************************************************
+ * FUNCTIONS
+ */
+
+ /*
+  * Initialization for the task
+  */
+extern void zclWaterCounter_Init( byte task_id );
+
+/*
+ *  Event Process for the task
+ */
+extern UINT16 zclWaterCounter_event_loop( byte task_id, UINT16 events );
+
+/*
+ *  Reset all writable attributes to their default values.
+ */
+extern void zclWaterCounter_ResetAttributesToDefaultValues(void); //implemented in zcl_watercounter_data.c
+
+/*********************************************************************
+*********************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZCL_SAMPLEAPP_H */
---
+++ Source/zcl_watercounter_data.c
@@ -0,0 +1,441 @@
+/**************************************************************************************************
+  Filename:       zcl_watercounter_data.c
+  Revised:        $Date: 2014-07-30 12:57:37 -0700 (Wed, 30 Jul 2014) $
+  Revision:       $Revision: 39591 $
+
+
+  Description:    Zigbee Cluster Library - sample device application.
+
+
+  Copyright 2006-2014 Texas Instruments Incorporated. All rights reserved.
+
+  IMPORTANT: Your use of this Software is limited to those specific rights
+  granted under the terms of a software license agreement between the user
+  who downloaded the software, his/her employer (which must be your employer)
+  and Texas Instruments Incorporated (the "License").  You may not use this
+  Software unless you agree to abide by the terms of the License. The License
+  limits your use, and you acknowledge, that the Software may not be modified,
+  copied or distributed unless embedded on a Texas Instruments microcontroller
+  or used solely and exclusively in conjunction with a Texas Instruments radio
+  frequency transceiver, which is integrated into your product.  Other than for
+  the foregoing purpose, you may not use, reproduce, copy, prepare derivative
+  works of, modify, distribute, perform, display or sell this Software and/or
+  its documentation for any purpose.
+
+  YOU FURTHER ACKNOWLEDGE AND AGREE THAT THE SOFTWARE AND DOCUMENTATION ARE
+  PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+  INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY, TITLE,
+  NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL
+  TEXAS INSTRUMENTS OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER CONTRACT,
+  NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR OTHER
+  LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+  INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE
+  OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT
+  OF SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+  (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+
+  Should you have any questions regarding your right to use this Software,
+  contact Texas Instruments Incorporated at www.TI.com.
+**************************************************************************************************/
+
+/*********************************************************************
+ * INCLUDES
+ */
+#include "ZComDef.h"
+#include "OSAL.h"
+#include "AF.h"
+#include "ZDConfig.h"
+
+#include "zcl.h"
+#include "zcl_general.h"
+#include "zcl_ha.h"
+#include "zcl_poll_control.h"
+#include "zcl_electrical_measurement.h"
+#include "zcl_diagnostic.h"
+#include "zcl_meter_identification.h"
+#include "zcl_appliance_identification.h"
+#include "zcl_appliance_events_alerts.h"
+#include "zcl_power_profile.h"
+#include "zcl_appliance_control.h"
+#include "zcl_appliance_statistics.h"
+#include "zcl_hvac.h"
+
+#include "zcl_watercounter.h"
+
+/*********************************************************************
+ * CONSTANTS
+ */
+
+#define WATERCOUNTER_DEVICE_VERSION     1
+#define WATERCOUNTER_FLAGS              0
+
+#define WATERCOUNTER_HWVERSION          0
+#define WATERCOUNTER_ZCLVERSION         0
+
+#define WATERCOUNTER_DESCSIZE           8
+#define WATERCOUNTER_UNITSIZE           4
+
+#define DEFAULT_PHYSICAL_ENVIRONMENT 0
+#define DEFAULT_DEVICE_ENABLE_STATE DEVICE_ENABLED
+#define DEFAULT_IDENTIFY_TIME 0
+
+/*********************************************************************
+ * TYPEDEFS
+ */
+
+/*********************************************************************
+ * MACROS
+ */
+
+/*********************************************************************
+ * GLOBAL VARIABLES
+ */
+
+//global attributes
+const uint16 zclWaterCounter_clusterRevision_all = 0x0001; //currently all cluster implementations are according to ZCL6, which has revision #1. In the future it is possible that different clusters will have different revisions, so they will have to use separate attribute variables.
+
+// Basic Cluster
+const uint8 zclWaterCounter_HWRevision = WATERCOUNTER_HWVERSION;
+const uint8 zclWaterCounter_ZCLVersion = WATERCOUNTER_ZCLVERSION;
+const uint8 zclWaterCounter_ManufacturerName[] = { 15, 'F','o','x','.','I','n','s','t','r','u','m','e','n','t','s' };
+const uint8 zclWaterCounter_ModelId[] = { 15, 'F','O','X','0','0','1',' ',' ',' ',' ',' ',' ',' ',' ',' ' };
+const uint8 zclWaterCounter_DateCode[] = { 15, '2','0','2','4','0','2','0','2',' ',' ',' ',' ',' ',' ',' ' };
+const uint8 zclWaterCounter_PowerSource = POWER_SOURCE_BATTERY;
+
+uint8    zclWaterCounter_Flow1Desc[WATERCOUNTER_DESCSIZE];
+uint16   zclWaterCounter_Flow1Multiplyer;
+uint8    zclWaterCounter_Flow1Unit[WATERCOUNTER_UNITSIZE];
+
+uint8    zclWaterCounter_Flow2Desc[WATERCOUNTER_DESCSIZE];
+uint16   zclWaterCounter_Flow2Multiplyer;
+uint8    zclWaterCounter_Flow2Unit[WATERCOUNTER_UNITSIZE];
+
+uint8 zclWaterCounter_LocationDescription[16];
+uint8 zclWaterCounter_PhysicalEnvironment;
+uint8 zclWaterCounter_DeviceEnable = DEVICE_ENABLED;
+
+// Identify Cluster
+uint16 zclWaterCounter_IdentifyTime = 0;
+
+/*********************************************************************
+ * ATTRIBUTE DEFINITIONS - Uses REAL cluster IDs
+ */
+ 
+  // NOTE: The attributes listed in the AttrRec must be in ascending order 
+  // per cluster to allow right function of the Foundation discovery commands
+ 
+CONST zclAttrRec_t zclWaterCounter_Attrs[] =
+{
+  // *** General Basic Cluster Attributes ***
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,
+    { // Attribute record
+      ATTRID_BASIC_ZCL_VERSION,
+      ZCL_DATATYPE_UINT8,
+      ACCESS_CONTROL_READ,
+      (void *)&zclWaterCounter_ZCLVersion
+    }
+  },  
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,             // Cluster IDs - defined in the foundation (ie. zcl.h)
+    {  // Attribute record
+      ATTRID_BASIC_HW_VERSION,            // Attribute ID - Found in Cluster Library header (ie. zcl_general.h)
+      ZCL_DATATYPE_UINT8,                 // Data Type - found in zcl.h
+      ACCESS_CONTROL_READ,                // Variable access control - found in zcl.h
+      (void *)&zclWaterCounter_HWRevision  // Pointer to attribute variable
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,
+    { // Attribute record
+      ATTRID_BASIC_MANUFACTURER_NAME,
+      ZCL_DATATYPE_CHAR_STR,
+      ACCESS_CONTROL_READ,
+      (void *)zclWaterCounter_ManufacturerName
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,
+    { // Attribute record
+      ATTRID_BASIC_MODEL_ID,
+      ZCL_DATATYPE_CHAR_STR,
+      ACCESS_CONTROL_READ,
+      (void *)zclWaterCounter_ModelId
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,
+    { // Attribute record
+      ATTRID_BASIC_DATE_CODE,
+      ZCL_DATATYPE_CHAR_STR,
+      ACCESS_CONTROL_READ,
+      (void *)zclWaterCounter_DateCode
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,
+    { // Attribute record
+      ATTRID_BASIC_POWER_SOURCE,
+      ZCL_DATATYPE_ENUM8,
+      ACCESS_CONTROL_READ,
+      (void *)&zclWaterCounter_PowerSource
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,
+    { // Attribute record
+      ATTRID_BASIC_LOCATION_DESC,
+      ZCL_DATATYPE_CHAR_STR,
+      (ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE),
+      (void *)zclWaterCounter_LocationDescription
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,
+    { // Attribute record
+      ATTRID_BASIC_PHYSICAL_ENV,
+      ZCL_DATATYPE_ENUM8,
+      (ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE),
+      (void *)&zclWaterCounter_PhysicalEnvironment
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,
+    { // Attribute record
+      ATTRID_BASIC_DEVICE_ENABLED,
+      ZCL_DATATYPE_BOOLEAN,
+      (ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE),
+      (void *)&zclWaterCounter_DeviceEnable
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_GEN_BASIC,
+    {  // Attribute record
+      ATTRID_CLUSTER_REVISION,
+      ZCL_DATATYPE_UINT16,
+      ACCESS_CONTROL_READ,
+      (void *)&zclWaterCounter_clusterRevision_all
+    }
+  },  
+
+  // *** Identify Cluster Attribute ***
+  {
+    ZCL_CLUSTER_ID_GEN_IDENTIFY,
+    { // Attribute record
+      ATTRID_IDENTIFY_TIME,
+      ZCL_DATATYPE_UINT16,
+      (ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE),
+      (void *)&zclWaterCounter_IdentifyTime
+    }
+  },  
+  {
+    ZCL_CLUSTER_ID_GEN_IDENTIFY,
+    {  // Attribute record
+      ATTRID_CLUSTER_REVISION,
+      ZCL_DATATYPE_UINT16,
+      ACCESS_CONTROL_READ | ACCESS_GLOBAL,
+      (void *)&zclWaterCounter_clusterRevision_all
+    }
+  },
+  // *** Flow measure Cluster *** //
+  {
+    ZCL_CLUSTER_ID_MS_FLOW_MEASUREMENT,
+    { // Attribute record
+      ATTRID_IOV_BASIC_DESCRIPTION,
+      ZCL_DATATYPE_CHAR_STR,
+      ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE,
+      (void *)&zclWaterCounter_Flow1Desc
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_MS_FLOW_MEASUREMENT,
+    { // Attribute record
+      ATTRID_IOV_BASIC_PRESENT_VALUE,
+      ZCL_DATATYPE_UINT32,
+      ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE,
+      (void *)&zclWaterCounter_Flow1Value
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_MS_FLOW_MEASUREMENT,
+    { // Attribute record
+      ATTRID_IOV_BASIC_RESOLUTION,
+      ZCL_DATATYPE_UINT16,
+      ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE,
+      (void *)&zclWaterCounter_Flow1Multiplyer
+    }
+  },
+    {
+    ZCL_CLUSTER_ID_MS_FLOW_MEASUREMENT,
+    { // Attribute record
+      ATTRID_IOV_BASIC_ENGINEERING_UNITS,
+      ZCL_DATATYPE_CHAR_STR,
+      ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE,
+      (void *)&zclWaterCounter_Flow1Unit
+    }
+  },
+{
+    ZCL_CLUSTER_ID_MS_FLOW_MEASUREMENT,
+    { // Attribute record
+      ATTRID_IOV_BASIC_STATUS_FLAG,
+      ZCL_DATATYPE_UINT8,
+      ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE,
+      (void *)&zclWaterCounter_Flow1Status
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_MS_FLOW_MEASUREMENT,
+    { // Attribute record
+      ATTRID_IOV_BASIC_DESCRIPTION,
+      ZCL_DATATYPE_CHAR_STR,
+      ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE,
+      (void *)&zclWaterCounter_Flow2Desc
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_MS_FLOW_MEASUREMENT,
+    { // Attribute record
+      ATTRID_IOV_BASIC_PRESENT_VALUE,
+      ZCL_DATATYPE_UINT32,
+      ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE,
+      (void *)&zclWaterCounter_Flow2Value
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_MS_FLOW_MEASUREMENT,
+    { // Attribute record
+      ATTRID_IOV_BASIC_RESOLUTION,
+      ZCL_DATATYPE_UINT16,
+      ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE,
+      (void *)&zclWaterCounter_Flow2Multiplyer
+    }
+  },
+    {
+    ZCL_CLUSTER_ID_MS_FLOW_MEASUREMENT,
+    { // Attribute record
+      ATTRID_IOV_BASIC_ENGINEERING_UNITS,
+      ZCL_DATATYPE_CHAR_STR,
+      ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE,
+      (void *)&zclWaterCounter_Flow2Unit
+    }
+  },
+{
+    ZCL_CLUSTER_ID_MS_FLOW_MEASUREMENT,
+    { // Attribute record
+      ATTRID_IOV_BASIC_STATUS_FLAG,
+      ZCL_DATATYPE_UINT8,
+      ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE,
+      (void *)&zclWaterCounter_Flow2Status
+    }
+  },
+  {
+    ZCL_CLUSTER_ID_MS_FLOW_MEASUREMENT,
+    {  // Attribute record
+      ATTRID_CLUSTER_REVISION,
+      ZCL_DATATYPE_UINT16,
+      ACCESS_CONTROL_READ | ACCESS_CLIENT,
+      (void *)&zclWaterCounter_clusterRevision_all
+    }
+  },
+  // *** Groups Cluster *** //
+  {
+    ZCL_CLUSTER_ID_GEN_GROUPS,
+    {  // Attribute record
+      ATTRID_CLUSTER_REVISION,
+      ZCL_DATATYPE_UINT16,
+      ACCESS_CONTROL_READ | ACCESS_CLIENT,
+      (void *)&zclWaterCounter_clusterRevision_all
+    }
+  }
+};
+
+uint8 CONST zclWaterCounter_NumAttributes = ( sizeof(zclWaterCounter_Attrs) / sizeof(zclWaterCounter_Attrs[0]) );
+
+/*********************************************************************
+ * SIMPLE DESCRIPTOR
+ */
+// This is the Cluster ID List and should be filled with Application
+// specific cluster IDs.
+const cId_t zclWaterCounter_InClusterList[] =
+{
+  ZCL_CLUSTER_ID_GEN_BASIC,
+  ZCL_CLUSTER_ID_GEN_IDENTIFY,
+  ZCL_CLUSTER_ID_MS_FLOW_MEASUREMENT
+};
+
+#define ZCLWATERCOUNTER_MAX_INCLUSTERS    ( sizeof( zclWaterCounter_InClusterList ) / sizeof( zclWaterCounter_InClusterList[0] ))
+
+const cId_t zclWaterCounter_OutClusterList[] =
+{
+  ZCL_CLUSTER_ID_GEN_IDENTIFY,
+  ZCL_CLUSTER_ID_MS_FLOW_MEASUREMENT,
+  ZCL_CLUSTER_ID_GEN_GROUPS,
+};
+
+#define ZCLWATERCOUNTER_MAX_OUTCLUSTERS   ( sizeof( zclWaterCounter_OutClusterList ) / sizeof( zclWaterCounter_OutClusterList[0] ))
+
+SimpleDescriptionFormat_t zclWaterCounter_SimpleDesc =
+{
+  WATERCOUNTER_ENDPOINT,                  //  int Endpoint;
+  ZCL_HA_PROFILE_ID,                    //  uint16 AppProfId[2];
+  ZCL_HA_DEVICEID_FLOW_SENSOR,          //  uint16 AppDeviceId[2];
+  WATERCOUNTER_DEVICE_VERSION,            //  int   AppDevVer:4;
+  WATERCOUNTER_FLAGS,                     //  int   AppFlags:4;
+  ZCLWATERCOUNTER_MAX_INCLUSTERS,         //  byte  AppNumInClusters;
+  (cId_t *)zclWaterCounter_InClusterList, //  byte *pAppInClusterList;
+  ZCLWATERCOUNTER_MAX_OUTCLUSTERS,        //  byte  AppNumInClusters;
+  (cId_t *)zclWaterCounter_OutClusterList //  byte *pAppInClusterList;
+};
+
+/*********************************************************************
+ * GLOBAL FUNCTIONS
+ */
+
+/*********************************************************************
+ * LOCAL FUNCTIONS
+ */
+
+/*********************************************************************
+ * @fn      zclWaterCounter_ResetAttributesToDefaultValues
+ *
+ * @brief   Reset all writable attributes to their default values.
+ *
+ * @param   none
+ *
+ * @return  none
+ */
+void zclWaterCounter_ResetAttributesToDefaultValues(void)
+{
+  const uint8 Desc1[WATERCOUNTER_DESCSIZE] = {WATERCOUNTER_DESCSIZE - 1, 'C', 'o', 'l', 'd', ' ', ' ', ' ' };
+  const uint8 Desc2[WATERCOUNTER_DESCSIZE] = {WATERCOUNTER_DESCSIZE - 1, 'H', 'o', 't', ' ', ' ', ' ', ' ' };
+  const uint8 Unit[WATERCOUNTER_UNITSIZE] = {WATERCOUNTER_UNITSIZE - 1, 'L', ' ', ' '};
+  int i;
+  
+  zclWaterCounter_LocationDescription[0] = 15;
+  for (i = 1; i <= 15; i++)
+  {
+    zclWaterCounter_LocationDescription[i] = ' ';
+  }
+  
+  zclWaterCounter_PhysicalEnvironment = DEFAULT_PHYSICAL_ENVIRONMENT;
+  zclWaterCounter_DeviceEnable = DEFAULT_DEVICE_ENABLE_STATE;
+  
+  zclWaterCounter_IdentifyTime = DEFAULT_IDENTIFY_TIME;
+  
+  osal_memcpy(zclWaterCounter_Flow1Desc, Desc1, WATERCOUNTER_DESCSIZE);
+  osal_memcpy(zclWaterCounter_Flow2Desc, Desc2, WATERCOUNTER_DESCSIZE);
+  osal_memcpy(zclWaterCounter_Flow1Unit, Unit, WATERCOUNTER_UNITSIZE);
+  osal_memcpy(zclWaterCounter_Flow2Unit, Unit, WATERCOUNTER_UNITSIZE);
+  
+  zclWaterCounter_Flow1Value = 0;
+  zclWaterCounter_Flow2Value = 0;
+  zclWaterCounter_Flow1Multiplyer = 10;
+  zclWaterCounter_Flow2Multiplyer = 10;
+  zclWaterCounter_Flow1Status = 1;
+  zclWaterCounter_Flow2Status = 1;
+}
+
+/****************************************************************************
+****************************************************************************/
+
+
