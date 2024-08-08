e7001fa42d8a03132b36bb131ff7fd666e7783bd
Sergey <sergey1fox@gmail.com>
2024-08-09 03:53:07 +0500

    Updated: Key codes to compile project

--- hal/include/hal_key.h
+++ hal/include/hal_key.h
@@ -71,9 +71,10 @@ extern "C"
 #define HAL_KEY_SW_2 0x04  // Joystick center
 #define HAL_KEY_SW_3 0x08  // Joystick left
 #define HAL_KEY_SW_4 0x10  // Joystick down
+#define HAL_KEY_SW_5 0x20  // Joystick down
 
-#define HAL_KEY_SW_6 0x20  // Button S1 if available
-#define HAL_KEY_SW_7 0x40  // Button S2 if available
+#define HAL_KEY_SW_6 0x40  // Button S1 if available
+#define HAL_KEY_SW_7 0x80  // Button S2 if available
 
 /* Joystick */
 #define HAL_KEY_UP     0x01  // Joystick up
